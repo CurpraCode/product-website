a646204604c82643249f00918e6f608286dca9ab
oyerohabib <oyerohabib@yahoo.com>
2022-07-27 13:52:37 +0100

    configured google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-DBBN3WC1C7");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
