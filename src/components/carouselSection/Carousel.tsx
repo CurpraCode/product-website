05b7c3a3c4697c42e013b7b13447af0c8c0b8ae2
Abdirisak Mohamed <abdirisakjr@gmail.com>
2022-12-20 21:46:45 +0300

    improve carousel

--- src/components/carouselSection/Carousel.tsx
+++ src/components/carouselSection/Carousel.tsx
@@ -8,16 +8,54 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
+import { IconType } from "react-icons/lib";
 
 const Carousel = () => {
+  const cards: { icon: IconType; title: string; body: string }[] = [
+    {
+      icon: MdLocationOn,
+      title: "Quality Interpreters",
+      body: "60+ Langauges available for interpretations",
+    },
+    {
+      icon: AiFillClockCircle,
+      title: "Instant Access",
+      body: "Access to both Video and Audio mean of interpretation",
+    },
+    {
+      icon: IoMdHeadset,
+      title: "Support",
+      body: "24/7 assistance for your need",
+    },
+    {
+      icon: AiFillClockCircle,
+      title: "Instant Access",
+      body: "Access to both Video and Audio mean of interpretation",
+    },
+    {
+      icon: MdPrivacyTip,
+      title: "Privacy",
+      body: "100% secured application for your own privacy",
+    },
+    {
+      icon: MdLocationOn,
+      title: "HIPAA",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
+
   return (
     <Stack>
-      <Flex bg="#F4F6F9" px="70px">
+      <Flex
+        bg="#F4F6F9"
+        bgGradient="linear(180deg, #FFF 50%, #F4F6F9 50%)"
+        py="10px"
+        px={[5, 70]}
+      >
         <Swiper
           spaceBetween={30}
           modules={[Navigation]}
           navigation
-          onSwiper={(swiper) => console.log(swiper)}
           breakpoints={{
             640: {
               width: 640,
@@ -29,21 +67,15 @@ const Carousel = () => {
             },
           }}
         >
-          <SwiperSlide>
-            <CarouselCard IconName={MdLocationOn} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CarouselCard IconName={AiFillClockCircle} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CarouselCard IconName={IoMdHeadset} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CarouselCard IconName={MdPrivacyTip} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CarouselCard IconName={MdPrivacyTip} />
-          </SwiperSlide>
+          {cards.map((card, index) => (
+            <SwiperSlide key={index}>
+              <CarouselCard
+                IconName={card.icon}
+                title={card.title}
+                body={card.body}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </Flex>
     </Stack>
--- src/components/carouselSection/CarouselCard.tsx
+++ src/components/carouselSection/CarouselCard.tsx
@@ -2,38 +2,34 @@ import React from "react";
 import { Stack, Image, Text, Heading, Icon, Flex } from "@chakra-ui/react";
 import { Card, CardBody } from "@chakra-ui/card";
 
-const CarouselCard = ({ IconName }) => {
+const CarouselCard = ({ IconName, title, body }) => {
   return (
     <Card
       maxW="sm"
       size="sm"
       px="30px"
       bg="white"
-      boxShadow="md"
+      boxShadow="2px 2px 1px rgba(64, 62, 80, 0.12)"
       height="500px"
       width="416px"
-      borderRadius="md"
-      border="1px solid lightgray"
+      borderRadius="xl"
+      // border="2px solid lightgray"
     >
       <CardBody>
-        <Stack align="center" justifyContent="center" gap="2">
+        <Stack alignItems="start" justifyContent="space-between" gap="2">
           <Stack mt="6" gap="2">
             <Flex justifyContent={"start"} alignItems="center" gap="5px">
               <Icon as={IconName} color="blue.400" boxSize={6} />
-              <Heading size="md">Living room Sofa</Heading>
+              <Heading size="md">{title}</Heading>
             </Flex>
-            <Text>
-              This sofa is perfect for modern tropical spaces, baroque inspired
-              spaces, earthy toned spaces and for people who love a chic design
-              with a sprinkle of vintage design.
-            </Text>
+            <Text>{body}</Text>
           </Stack>
           <Image
             src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            alt="Green double couch with wooden legs"
+            alt="Green double couch"
             borderRadius="lg"
-            width="100%"
-            height="130px"
+            width="350px"
+            height="300px"
           />
         </Stack>
       </CardBody>
