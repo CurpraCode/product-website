fb591717e8f45e1c2531acfbce11a7464b4d28d5
Curtis Oyakoya <curtis@asfi.io>
2023-01-05 19:06:46 +0100

    update the how it work

--- src/components/howItWorksPage/TabSection.tsx
+++ src/components/howItWorksPage/TabSection.tsx
@@ -75,7 +75,33 @@ const TabSection = () => {
           </VStack>
         </TabPanel>
         <TabPanel>
-          <p>two!</p>
+          <VStack alignItems="center" gap="100px">
+            {TabsData.map((item, index) => (
+              <Stack
+                key={index}
+                w={["240px", "540px", "760px"]}
+                gap={["20px", "30px", "30px", "100px"]}
+                direction={["column", "column", "row"]}
+                alignItems={["center"]}
+                px={["0", "10px", "10px", "0"]}
+              >
+                <Image src="/random-gradient.svg" alt="gradient-box" />
+                <VStack
+                  alignItems={["center", "center", "start"]}
+                  textAlign={["center", "center", "start"]}
+                  gap="4"
+                >
+                  <Heading fontSize={["18px", "24px"]}>{item.title}</Heading>
+                  <Box
+                    border="4px solid #3A76BF"
+                    w="40px"
+                    borderRadius="10"
+                  ></Box>
+                  <Text fontSize="18px">{item.body}</Text>
+                </VStack>
+              </Stack>
+            ))}
+          </VStack>
         </TabPanel>
       </TabPanels>
     </Tabs>
